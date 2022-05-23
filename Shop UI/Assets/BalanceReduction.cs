d39022089d5acceb266c4cbf2af0129c512db7aa
tayyab34 <ahmedtayyab034@gmail.com>
2022-05-23 13:30:00 +0500

    Shop UI

--- Shop UI/Assets/BalanceReduction.cs
+++ Shop UI/Assets/BalanceReduction.cs
@@ -2,31 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class BalanceReduction : MonoBehaviour
 {
     public TextMeshProUGUI textUI;
-    private int Balance = 5000;
-    private int AmounttoDeduct =100;
+    private int Balance=5000;
     public GameObject purchased;
+    //public List<TextMeshProUGUI> TextList;
+    public int[] itemPrice;
     // Start is called before the first frame update
     void Start()
     {
-        textUI.text = "Balance: $" + Balance;
+        //textUI.text = "Balance: $" + Balance;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (purchased.activeInHierarchy)
         {
             StartCoroutine(Delay());
         }
     }
-    public void MoneyDeduction()
+    public void MoneyDeduction(int i)
     {
-        Balance -= AmounttoDeduct;
-        textUI.text = "Balance: $" + Balance;
+        //foreach (TextMeshProUGUI c in TextList)
+        //{
+        //    //var Balance = 5000;
+
+        //    //TextMeshProUGUI value = c.GetComponentInChildren<TextMeshProUGUI>();
+        //    int AmounttoDeduct =c.text;
+        //    textUI.text = "Balance: " + AmounttoDeduct;
+        //}
+        Balance -= itemPrice[i];
+        textUI.text = "Balance: " + Balance;
 
     }
     IEnumerator Delay()
